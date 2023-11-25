2f4415f2e732c7faf01d7cfb7f67581a1e152111
Leonardo Morselli <leonardo.morselli02@gmail.com>
2023-11-25 17:27:15 +0100

    feat(powerups): funzionante

---
+++ Assets/Scripts/Player/Stats.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Stats : MonoBehaviour
+{
+    Dictionary<PowerUpTypes, float> stats;
+
+
+    public UnityEvent OnModifyStats;
+
+    void Start()
+    {
+        if (OnModifyStats == null)
+            OnModifyStats = new UnityEvent();
+
+        stats = new Dictionary<PowerUpTypes, float>()
+        {
+            { PowerUpTypes.cooldownReduction, 10f },
+            { PowerUpTypes.laserDuration, 3f },
+            { PowerUpTypes.laserNumber,  1f },
+            { PowerUpTypes.health,  20f },
+            { PowerUpTypes.movementSpeed, 10f }
+        };
+    }
+
+    public void UpdateStat(Powerup powerup)
+    {
+        this.stats[powerup.type] = powerup.value;
+        OnModifyStats.Invoke();
+        Debug.Log("UPGRADED: " + powerup.UpgradeName + " - " + this.stats[powerup.type]);
+    }
+
+    public float GetStat(PowerUpTypes type)
+    {
+        return this.stats[type];
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject StatUpgradeUI;
+    [SerializeField]
+    TMP_Text button1Text;
+
+    [SerializeField]
+    TMP_Text button2Text;
+
+    [SerializeField]
+    TMP_Text button3Text;
+
+    [SerializeField]
+    Button Button1;
+    [SerializeField]
+    Button Button2;
+    [SerializeField]
+    Button Button3;
+
+    Stats stats;
+
+    bool GameIsPaused = false;
+
+    void Start()
+    {
+        this.stats = GameObject.FindGameObjectWithTag("Player").GetComponent<Stats>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (GameIsPaused)
+                Resume();
+            else if (!GameIsPaused)
+                Pause();
+
+
+        }
+    }
+
+    public void Pause()
+    {
+        // GameManager.Instance.SetIsUIVisible(true);
+        //pauseMenuUI.SetActive(true);
+        //overlayUI.SetActive(false);
+
+        StatUpgradeUI.SetActive(true);
+
+        Powerup upgrade1 = new Powerup(stats);
+        Powerup upgrade2 = new Powerup(stats);
+        Powerup upgrade3 = new Powerup(stats);
+        upgrade1.RandomizeSetup();
+        upgrade2.RandomizeSetup();
+        upgrade3.RandomizeSetup();
+
+        this.button1Text.text = upgrade1.UpgradeName;
+        this.button2Text.text = upgrade2.UpgradeName;
+        this.button3Text.text = upgrade3.UpgradeName;
+
+        Button1.onClick.AddListener(delegate { this.stats.UpdateStat(upgrade1); });
+        Button2.onClick.AddListener(delegate { this.stats.UpdateStat(upgrade2); });
+        Button3.onClick.AddListener(delegate { this.stats.UpdateStat(upgrade3); });
+
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        // pauseAnimator.SetBool("Open", false);
+        Time.timeScale = 1f;
+        StatUpgradeUI.SetActive(false);
+        GameIsPaused = false;
+
+    }
+
+    public void RemoveAllListener()
+    {
+        Button1.onClick.RemoveAllListeners();
+        Button1.onClick.AddListener(Resume);
+        Button1.onClick.AddListener(RemoveAllListener);
+
+        Button2.onClick.RemoveAllListeners();
+        Button2.onClick.AddListener(Resume);
+        Button2.onClick.AddListener(RemoveAllListener);
+
+        Button3.onClick.RemoveAllListeners();
+        Button3.onClick.AddListener(Resume);
+        Button2.onClick.AddListener(RemoveAllListener);
+
+    }
+
+}
